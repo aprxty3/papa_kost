2050f8c38054a6189cb209fe26d95e1cc846d30f
aprxrt3 <ajiprxty3@gmail.com>
2021-12-10 13:53:31 +0700

    add route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:papa_kost/pages/splash_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: splashScreen(),
     );
   }
 }
