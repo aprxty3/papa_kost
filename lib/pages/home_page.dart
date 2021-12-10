5fcda2bd3ae41fa4a038c97e18d6f996027978a1
aprxrt3 <ajiprxty3@gmail.com>
2021-12-10 21:16:25 +0700

    bugfix

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:papa_kost/theme.dart';
 import 'package:papa_kost/widget/kota_cart.dart';
+import 'package:papa_kost/model/ko_model.dart';
 
 class homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget header() {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Explore Now',
-            style: TopStyle,
-          ),
-          SizedBox(
-            height: 2,
-          ),
-          Text(
-            'Mencari kosan yang cozy',
-            style: MedStyle,
-          ),
-        ],
+      return Padding(
+        padding: const EdgeInsets.only(
+          left: 24,
+          top: 24,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Explore Now',
+              style: TopStyle,
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Text(
+              'Mencari kosan yang cozy',
+              style: MedStyle,
+            ),
+          ],
+        ),
       );
     }
 
@@ -31,9 +38,14 @@ class homepage extends StatelessWidget {
           SizedBox(
             height: 30,
           ),
-          Text(
-            'Popular Cities',
-            style: RTTGStyle,
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 24,
+            ),
+            child: Text(
+              'Popular Cities',
+              style: RTTGStyle,
+            ),
           ),
           SizedBox(
             height: 16,
@@ -43,30 +55,57 @@ class homepage extends StatelessWidget {
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: [
-                KoCart(),
-                KoCart(),
-                KoCart(),
+                SizedBox(
+                  width: 24,
+                ),
+                KoCart(
+                  Kota(
+                    id: 1,
+                    name: 'Jakarta',
+                    imageUrl: 'assets/city1.png',
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                KoCart(
+                  Kota(
+                    id: 2,
+                    name: 'Bandung',
+                    imageUrl: 'assets/city2.png',
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                KoCart(
+                  Kota(
+                    id: 3,
+                    name: 'Surabaya',
+                    imageUrl: 'assets/city3.png',
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                SizedBox(
+                  width: 24,
+                ),
               ],
             ),
-          )
+          ),
         ],
       );
     }
 
     return Scaffold(
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: 24,
-            top: 24,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              header(),
-              popularCity(),
-            ],
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            header(),
+            popularCity(),
+          ],
         ),
       ),
     );
