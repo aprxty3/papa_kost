eb997088d76e74c69a7ee2003fd5f4937b697c26
aprxrt3 <ajiprxty3@gmail.com>
2021-12-10 14:19:22 +0700

    add header

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:papa_kost/theme.dart';
 
 class splashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget Header() {
-      return SafeArea(
+      return Padding(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 30,
+          right: 60,
+        ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               width: 50,
@@ -16,17 +23,57 @@ class splashScreen extends StatelessWidget {
                 ),
               ),
             ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Find Cozy House \nto Stay and Happy',
+              style: TopStyle,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Stop membuang banyak waktu \npada tempat yang tidak habitable',
+              style: MedStyle,
+            ),
           ],
         ),
       );
     }
 
+    Widget ExploreButton() {
+      return Padding(
+        padding: EdgeInsets.only(top: 40, left: 30),
+        child: Container(
+          width: 210,
+          height: 50,
+          child: TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: Color(0xff5843BE),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(17),
+              ),
+            ),
+            onPressed: () {},
+            child: Text(
+              'Explore Now',
+              style: TBtStyle,
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
-      body: ListView(
+      body: SingleChildScrollView(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Header(),
+          ExploreButton(),
         ],
-      ),
+      )),
     );
   }
 }
