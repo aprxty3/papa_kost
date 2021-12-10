ec892574130c0f0c1f8098d09be4aa49d8f4bda8
aprxrt3 <ajiprxty3@gmail.com>
2021-12-10 17:11:15 +0700

    add header

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:papa_kost/theme.dart';
 
 class homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Widget header() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Explore Now',
+            style: TopStyle,
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          Text(
+            'Mencari kosan yang cozy',
+            style: MedStyle,
+          ),
+        ],
+      );
+    }
+
+    Widget popularCity() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Popular Cities',
+            style: RTTGStyle,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Image.asset(
+                    'assets/city1.png',
+                    width: 120,
+                    height: 102,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Jakarta',
+                    style: KotaStyle,
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+      );
+    }
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 24,
+            top: 24,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              header(),
+              popularCity(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
