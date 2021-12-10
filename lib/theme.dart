6e2bdddeca92f56e8f3066fd31a76a3b81764eb0
aprxrt3 <ajiprxty3@gmail.com>
2021-12-10 13:27:37 +0700

    add theme textstyle

---
+++ lib/theme.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//SPLASH
+
+TextStyle TopStyle = GoogleFonts.poppins(
+  fontSize: 24,
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
+
+TextStyle MedStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+  color: Color(0xff82868E),
+);
+
+TextStyle TBtStyle = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
+);
+
+//HOME
+
+TextStyle RTTGStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: Colors.black,
+);
+
+TextStyle KotaStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
+
+TextStyle KostStyle = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
+
+TextStyle PriceStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: Color(0xff5843BE),
+);
+
+TextStyle MonthStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+  color: Color(0xff7A7E86),
+);
+
+TextStyle LocStyle = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+  color: Color(0xff7A7E86),
+);
