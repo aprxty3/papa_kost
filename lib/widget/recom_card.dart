d2833c54c3a1503705331e3e83bcfd14e88406ff
aprxrt3 <ajiprxty3@gmail.com>
2021-12-12 06:02:59 +0700

    add navigator

--- lib/widget/recom_card.dart
+++ lib/widget/recom_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:papa_kost/model/re_model.dart';
+import 'package:papa_kost/pages/detail_page.dart';
 import 'package:papa_kost/theme.dart';
 
 class ReCard extends StatelessWidget {
@@ -9,86 +10,96 @@ class ReCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(18),
-          child: Container(
-            width: 130,
-            height: 110,
-            child: Stack(
-              children: [
-                Image.asset(
-                  recom.imageUrl,
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    width: 70,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      color: Color(0xff5843BE),
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(30),
-                      ),
-                    ),
-                    child: Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/icon_star.png',
-                            width: 22,
-                            height: 22,
-                          ),
-                          Text(
-                            recom.bintang,
-                            style: Rate,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailPage(),
           ),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              recom.name,
-              style: KostStyle,
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Text.rich(
-              TextSpan(
-                text: '\$${recom.price}',
-                style: PriceStyle,
+        );
+      },
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: Container(
+              width: 130,
+              height: 110,
+              child: Stack(
                 children: [
-                  TextSpan(
-                    text: ' / Month',
-                    style: MonthStyle,
+                  Image.asset(
+                    recom.imageUrl,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 70,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: Color(0xff5843BE),
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(30),
+                        ),
+                      ),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/icon_star.png',
+                              width: 22,
+                              height: 22,
+                            ),
+                            Text(
+                              recom.bintang,
+                              style: Rate,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              recom.location,
-              style: LocStyle,
-            ),
-          ],
-        )
-      ],
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                recom.name,
+                style: KostStyle,
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text.rich(
+                TextSpan(
+                  text: '\$${recom.price}',
+                  style: PriceStyle,
+                  children: [
+                    TextSpan(
+                      text: ' / Month',
+                      style: MonthStyle,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                recom.location,
+                style: LocStyle,
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
