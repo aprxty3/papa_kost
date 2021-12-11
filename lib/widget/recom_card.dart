79b36775ab71e685f3c071ddac874f0136ce608f
aprxrt3 <ajiprxty3@gmail.com>
2021-12-11 15:13:43 +0700

    chore

--- lib/widget/recom_card.dart
+++ lib/widget/recom_card.dart
@@ -1,48 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:papa_kost/model/re_model.dart';
 import 'package:papa_kost/theme.dart';
 
 class ReCard extends StatelessWidget {
+  final Recom recom;
+
+  ReCard(this.recom);
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(
-          width: 130,
-          height: 110,
-          child: Stack(
-            children: [
-              Image.asset(
-                'assets/space1.png',
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Container(
-                  width: 70,
-                  height: 30,
-                  decoration: BoxDecoration(
-                    color: Color(0xff5843BE),
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(18),
+          child: Container(
+            width: 130,
+            height: 110,
+            child: Stack(
+              children: [
+                Image.asset(
+                  'assets/space1.png',
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: 70,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Color(0xff5843BE),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                      ),
                     ),
-                  ),
-                  child: Center(
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          'assets/icon_star.png',
-                          width: 22,
-                          height: 22,
-                        ),
-                        Text(
-                          '4/5',
-                          style: Rate,
-                        ),
-                      ],
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icon_star.png',
+                            width: 22,
+                            height: 22,
+                          ),
+                          Text(
+                            '4/5',
+                            style: Rate,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         )
       ],
