9090cc260028b6707caefeeec67b19654587935b
aprxrt3 <ajiprxty3@gmail.com>
2021-12-11 19:03:19 +0700

    create bottom nav widget

---
+++ lib/widget/bottomnav.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class BotItem extends StatelessWidget {
+  final String imageUrl;
+  final bool isActive;
+
+  BotItem({
+    this.imageUrl,
+    this.isActive,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Spacer(),
+        Image.asset(
+          imageUrl,
+          width: 26,
+          height: 26,
+        ),
+        Spacer(),
+        isActive
+            ? Container(
+                width: 30,
+                height: 2,
+                decoration: BoxDecoration(
+                  color: Color(0xff7F74EB),
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(1000),
+                  ),
+                ),
+              )
+            : Container(),
+      ],
+    );
+  }
+}
