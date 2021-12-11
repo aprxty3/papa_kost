2d56194c252d81c75b4266465e2fd4d858e65d67
aprxrt3 <ajiprxty3@gmail.com>
2021-12-11 15:13:37 +0700

    create recom model

---
+++ lib/model/re_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Recom {
+  String imageUrl;
+  String name;
+  int price;
+  String bulan;
+  String location;
+
+  Recom({
+    this.imageUrl,
+    this.name,
+    this.price,
+    this.bulan,
+    this.location,
+  });
+}
