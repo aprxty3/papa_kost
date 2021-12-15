146dc0c87f08fa82264e2e3e69de536b02980ec5
aprxrt3 <ajiprxty3@gmail.com>
2021-12-15 14:21:10 +0700

    add change notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:papa_kost/pages/splash_screen.dart';
+import 'package:papa_kost/provider/recom_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => RecomProv(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      ),
     );
   }
 }
