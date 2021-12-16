c2dbf82627c8aec4e6372f2f1302a9d298dffad8
aprxrt3 <ajiprxty3@gmail.com>
2021-12-16 16:32:59 +0700

    bugfix

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -36,37 +36,40 @@ class DetailPage extends StatelessWidget {
             height: 350,
             fit: BoxFit.cover,
           ),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 20,
-              left: 24,
-              right: 24,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => homepage()),
-                    );
-                  },
-                  child: Image.asset(
-                    'assets/btn_back.png',
-                    width: 40,
-                    height: 40,
-                  ),
-                ),
-                Image.asset(
-                  'assets/btn_wishlist.png',
-                  width: 40,
-                  height: 40,
-                ),
-              ],
-            ),
-          ),
         ],
+      );
+    }
+
+    Widget button() {
+      return Padding(
+        padding: const EdgeInsets.only(
+          top: 20,
+          left: 24,
+          right: 24,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => homepage()),
+                );
+              },
+              child: Image.asset(
+                'assets/btn_back.png',
+                width: 40,
+                height: 40,
+              ),
+            ),
+            Image.asset(
+              'assets/btn_wishlist.png',
+              width: 40,
+              height: 40,
+            ),
+          ],
+        ),
       );
     }
 
@@ -336,10 +339,10 @@ class DetailPage extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
-        bottom: false,
         child: Stack(
           children: [
             header(),
+            button(),
             content(),
           ],
         ),
